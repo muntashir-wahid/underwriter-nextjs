bf4caa627e74954142bd0398e08c1992fa3c3593
muntashir-wahid <muntashir.wahid@yahoo.com>
2024-03-31 17:57:04 +0600

    created nextjs project and started working on the task

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl text-red-400 text-center font-semibold">
+        Let's start!
+      </h1>
+    </main>
+  );
+}
