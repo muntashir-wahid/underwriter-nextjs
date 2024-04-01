942867d6b6e07fa6eb246c2ff83626dcdb2eea17
muntashir-wahid <muntashir.wahid@yahoo.com>
2024-04-01 06:16:33 +0600

    completed top navigation for all screen size

--- components/Global/TopNavigation/Nav.jsx
+++ components/Global/TopNavigation/Nav.jsx
@@ -1,5 +1,12 @@
-import Button from "@/components/Shared/Buttons/Button";
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { FaXmark } from "react-icons/fa6";
+
+import Button from "@/components/Shared/Buttons/Button";
+import NavDropDownMenu from "./NavDropDownMenu";
 
 const navigationContents = [
   { title: "Home", link: "/" },
@@ -9,10 +16,11 @@ const navigationContents = [
 ];
 
 export default function Nav() {
+  const [showNavMenu, setShowNavMenu] = useState(false);
   return (
     <nav className="py-4 border-b">
-      {/* Nav for bigger screen */}
-      <div className="hidden max-w-7xl mx-auto px-2 md:flex items-center justify-between">
+      {/* Nav for larger screen */}
+      <div className="hidden max-w-7xl mx-auto md:flex px-2 items-center justify-between">
         <figure>
           <img
             src="assets/images/logo.png"
@@ -20,18 +28,41 @@ export default function Nav() {
             className="h-8 w-20"
           />
         </figure>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-1">
           {navigationContents.map((navContent) => (
             <Link
               key={navContent.title}
               href={navContent.link}
-              className="text-gray-900 font-medium"
+              className="text-gray-900 font-medium px-3"
             >
               {navContent.title}
             </Link>
           ))}
           <Button variant="red">Report a Claim</Button>
         </div>
+      </div>
+
+      {/* Nav for smaller screen */}
+      <div className="w-full flex md:hidden justify-end relative">
+        <div className="w-2/3 sm:w-1/2 flex items-center justify-between px-2">
+          <figure>
+            <img
+              src="assets/images/logo.png"
+              alt="USA Underwriter Logo"
+              className="h-8 w-20"
+            />
+          </figure>
+          <button onClick={() => setShowNavMenu((prevState) => !prevState)}>
+            {showNavMenu ? (
+              <FaXmark size="24" className="text-gray-900" />
+            ) : (
+              <HiMenuAlt3 size="24" className="text-gray-900" />
+            )}
+          </button>
+        </div>
+        {showNavMenu && (
+          <NavDropDownMenu navigationContents={navigationContents} />
+        )}
       </div>
     </nav>
   );
---
+++ components/Global/TopNavigation/NavDropDownMenu.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function NavDropDownMenu({ navigationContents }) {
+  return (
+    <div className="w-full absolute flex flex-col items-start top-12 bg-red-700">
+      {navigationContents.map((navContent) => (
+        <Link
+          key={navContent.title}
+          href={navContent.link}
+          className="text-white font-medium py-4 pl-8"
+        >
+          {navContent.title}
+        </Link>
+      ))}
+    </div>
+  );
+}
