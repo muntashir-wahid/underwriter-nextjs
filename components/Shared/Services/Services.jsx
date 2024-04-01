5c9b4af3c91e02a74c4c243aa93ba77922131d0b
muntashir-wahid <muntashir.wahid@yahoo.com>
2024-04-01 07:21:44 +0600

    completed report section

--- components/Shared/Services/Services.jsx
+++ components/Shared/Services/Services.jsx
@@ -1,3 +1,32 @@
+import ServiceCard from "./ServiceCard";
+
+const servicesContent = [
+  {
+    id: "1",
+    heading: "Affordable Auto Insurance",
+    desc: "USA Underwriters offers affordable rates on quality coverage.",
+    imageSrc: "assets/images/service-img.png",
+  },
+  {
+    id: "2",
+    heading: "Accidents Can Add Up",
+    desc: "Accidents and infractions can linger long after the fines are paid and the car is repaired.",
+    imageSrc: "assets/images/service-img.png",
+  },
+  {
+    id: "3",
+    heading: "Price Conscious",
+    desc: "Price conscious consumers choose USA Underwriters for affordable auto insurance solutions.",
+    imageSrc: "assets/images/service-img.png",
+  },
+];
+
 export default function Services() {
-  return <section>Services section</section>;
+  return (
+    <section className="mt-10 grid md:grid-cols-2 lg:grid-cols-3 gap-3">
+      {servicesContent.map((service) => (
+        <ServiceCard key={service.id} service={service} />
+      ))}
+    </section>
+  );
 }
